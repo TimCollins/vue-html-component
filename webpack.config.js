43e4b683129cbf1806ac524d18659f58588cf239
Tim <tim@timcollins.ie>
2018-12-01 21:53:47 +0000

    Added configuration to parse fonts, html and js.

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,39 @@ module.exports = {
   mode: 'development',
   devServer: {
     contentBase: path.join(__dirname, '/src')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      { 
+        test: /\.html$/, 
+        loader: 'vue-template-compiler-loader' 
+      },
+      {
+        test: /\.scss$/,
+        exclude: [/node_modules/],
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      }
+    ]
   }
-}
+};
